5eec8aa87539aa433cd63067a0e1cbac1329f35e
anayatc@gmail.com <anayatc@sky.com>
2017-05-02 16:32:38 +0100

    Automated Testing

--- Skeleton/ex47/game.py
+++ Skeleton/ex47/game.py
@@ -4,9 +4,13 @@ class Room(object):
         self.name = name
         self.description = description
         self.paths = {}
+        self.weapon = {}
 
     def go(self, direction):
         return self.paths.get(direction, None)
 
     def add_paths(self, paths):
         self.paths.update(paths)
+
+    def weapons(self, weapon):
+        self.weapon.update(weapon)
